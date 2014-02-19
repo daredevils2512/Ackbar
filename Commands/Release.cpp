56b03d60119968659eea147e63bcf62ab632f6d3
Daredevils <daredevils2512@gmail.com>
2014-02-18 18:23:22 -0600

    Removed shooting safety measures.

--- Commands/Release.cpp
+++ Commands/Release.cpp
@@ -32,7 +32,6 @@ bool Release::IsFinished() {
 // Called once after isFinished returns true
 void Release::End() {
 	Robot::shooter->SetSolenoids(false);
-	Robot::shooter->NotLoaded();
 }
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
